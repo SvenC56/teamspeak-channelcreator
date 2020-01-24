444acac33bccf324b2bcde8d3052dae32a74722a
Sven Cieslok <mail@svencieslok.de>
2020-01-24 23:00:58 +0100

    remove colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-const colors = require('vuetify/es5/util/colors').default
-
 module.exports = {
   mode: 'universal',
   /*
@@ -51,10 +49,6 @@ module.exports = {
    */
   axios: {
     axios: {
-      browserBaseURL:
-        process.env.NODE_ENV === 'production'
-          ? process.env.BASE_URL
-          : 'http://0.0.0.0',
       baseUrl:
         process.env.NODE_ENV === 'production'
           ? process.env.BASE_URL
@@ -68,18 +62,7 @@ module.exports = {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      dark: true
     }
   },
   /*
